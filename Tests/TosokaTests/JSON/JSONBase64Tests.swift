257c5e0d43fa540bcaee368a8287b6b9e323d188
Igor Nikitin <rabbitinspace@icloud.com>
2017-01-02 23:13:17 +0300

    JSON Base64 tests

---
+++ Tests/TosokaTests/JSON/JSONBase64Tests.swift
@@ -0,0 +1,116 @@
+import Foundation
+import XCTest
+@testable import Tosoka
+
+class JSONBase64Tests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    var base64Coder: Base64<URLSafeCodingAlphabet>!
+    var jsonCoder: JSONCoder!
+    
+    // MARK: - Preparations
+    
+    override func setUp() {
+        super.setUp()
+        
+        base64Coder = Base64(coder: URLSafeCodingAlphabet.self, isPaddingEnabled: false)
+        #if os(macOS)
+            jsonCoder = JSONCoder()
+        #else
+            jsonCoder = LinuxJSONCoder()
+        #endif
+    }
+    
+    // MARK: - Tests
+    
+    func testHeaderJSON() {
+        let header = [
+            "alg": "HS256",
+            "typ": "JWT"
+        ]
+        
+        guard let headerData = jsonCoder.makeData(with: header) else {
+            XCTFail()
+            return
+        }
+        
+        let systemEncodedHeaderString = encode(headerData)
+        let customEncodedHeaderData = base64Coder.encode(headerData)
+        
+        XCTAssertEqual(
+            systemEncodedHeaderString,
+            String(data: customEncodedHeaderData, encoding: .utf8)
+        )
+        
+        guard let systemDecodedHeaderData = decode(systemEncodedHeaderString),
+            let customDecodedHeaderData = try? base64Coder.decode(customEncodedHeaderData) else {
+                XCTFail()
+                return
+        }
+        
+        XCTAssertEqual(systemDecodedHeaderData, customDecodedHeaderData)
+        XCTAssertEqual(systemDecodedHeaderData, headerData)
+        XCTAssertEqual(customDecodedHeaderData, headerData)
+        
+        guard let headerFromSystemDecoder = jsonCoder.makeJSON(with: systemDecodedHeaderData),
+            let headerFromCustomDecoder = jsonCoder.makeJSON(with: customDecodedHeaderData) else {
+                XCTFail()
+                return
+        }
+        
+        XCTAssertEqual(
+            headerFromCustomDecoder["alg"] as? String,
+            headerFromSystemDecoder["alg"] as? String
+        )
+        
+        XCTAssertEqual(
+            headerFromCustomDecoder["typ"] as? String,
+            headerFromSystemDecoder["typ"] as? String
+        )
+        
+        XCTAssertEqual(
+            headerFromCustomDecoder["alg"] as? String,
+            header["alg"]
+        )
+        
+        XCTAssertEqual(
+            headerFromCustomDecoder["typ"] as? String,
+            header["typ"]
+        )
+    }
+    
+    // MARK: - Base64 encoding/decoding
+    
+    // from rfc7515, Appendix C
+    private func encode(_ data: Data) -> String {
+        let encodedString = data.base64EncodedString().components(separatedBy: "=")[0]
+        return encodedString
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+    }
+    
+    // from rfc7515, Appendix C
+    private func decode(_ string: String) -> Data? {
+        let encodedString = string
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        
+        let stringToDecode: String
+        switch encodedString.utf8.count % 4 {
+        case 0, 2, 3:
+            stringToDecode = encodedString
+            
+        case 2:
+            stringToDecode = encodedString.appending("==")
+            
+        case 3:
+            stringToDecode = encodedString.appending("=")
+            
+        default:
+            return nil
+        }
+        
+        return Data(base64Encoded: stringToDecode)
+    }
+}
